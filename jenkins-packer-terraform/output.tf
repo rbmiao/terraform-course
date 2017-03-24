ba7f49c2a799a874a5715a188f9b97f7599e24ce
rbmiao <rbmiao@gmail.com>
2017-03-24 20:06:23 +0000

    change output variables

--- jenkins-packer-terraform/output.tf
+++ jenkins-packer-terraform/output.tf
@@ -1,11 +1,3 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name = "mykeypair"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-  lifecycle {
-    ignore_changes = ["public_key"]
-  }
-}
-root@inode2:~/terraform-course/terraform-course/jenkins-packer-demo# cat output.tf
 output "jenkins-ip" {
   value = "${aws_instance.jenkins-instance.public_ip}"
 }
